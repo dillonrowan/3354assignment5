2fdd3414a3b55472f5e565384843464c1264929e
Bruno Blanco <32379134+BBBlanco@users.noreply.github.com>
2017-11-21 00:36:09 -0600

    Deliver package complete

--- shippingstore/Validate.java
+++ shippingstore/Validate.java
@@ -2,6 +2,13 @@ package shippingstore;
 
 import java.io.*;
 import java.util.*;
+import java.text.ParseException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Validate {
 
@@ -23,5 +30,19 @@ public class Validate {
       return false;
 
     return true;
+  }
+
+  public static Date parseDate(String maybeDate) {
+    Date date = null;
+    String format = "MM/dd/yyyy";
+    String reFormat = Pattern.compile("d+|M+").matcher(Matcher.quoteReplacement(format)).replaceAll("\\\\d{1,2}");
+    reFormat = Pattern.compile("y+").matcher(reFormat).replaceAll("\\\\d{4}");
+    if ( Pattern.compile(reFormat).matcher(maybeDate).matches() ) {
+      SimpleDateFormat sdf = (SimpleDateFormat)DateFormat.getDateInstance();
+      sdf.applyPattern(format);
+      sdf.setLenient(false);
+      try { date = sdf.parse(maybeDate); } catch (ParseException e) { }
+    }
+    return date;
   }
 }
